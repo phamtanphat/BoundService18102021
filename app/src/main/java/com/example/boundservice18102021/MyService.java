a65abe364e682ca7ab5369d1f4f15f44951da3fa
phamtanphat <phatdroid94@gmail.com>
2022-01-05 17:55:24 +0700

    setup foreground

--- app/src/main/java/com/example/boundservice18102021/MyService.java
+++ app/src/main/java/com/example/boundservice18102021/MyService.java
@@ -1,11 +1,13 @@
 package com.example.boundservice18102021;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
@@ -14,6 +16,9 @@ public class MyService extends Service {
 
     String CHANNEL_ID = "CHANNEL_ID";
     int REQUEST_CODE_COUNT = 1;
+    Notification mNotification;
+    int mCount = 0;
+    NotificationManager mNotificationManager;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -23,15 +28,21 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        mNotification = createNotification(this,"Count : " + mCount);
+        startForeground(1,mNotification);
+        mNotificationManager.notify(1,mNotification);
+        Log.d("BBB","onCreate");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        Log.d("BBB","onStartCommand");
+        return START_NOT_STICKY;
     }
 
     @Override
     public void onDestroy() {
+        Log.d("BBB","onDestroy");
         super.onDestroy();
     }
 
