8cdd36fc19626c1ce87287bbccf28133827a7d5a
phamtanphat <phatdroid94@gmail.com>
2022-01-05 18:47:57 +0700

    declare

--- app/src/main/java/com/example/boundservice18102021/MainActivity.java
+++ app/src/main/java/com/example/boundservice18102021/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.boundservice18102021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -33,5 +37,30 @@ public class MainActivity extends AppCompatActivity {
                 stopService(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(MainActivity.this,MyService.class);
+        bindService(intent,connection,BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
     }
 }
