c4401fa0b58380456264c53811daa043f376ee9f
phamtanphat <phatdroid94@gmail.com>
2022-01-05 18:39:59 +0700

    binder

--- app/src/main/java/com/example/boundservice18102021/MyService.java
+++ app/src/main/java/com/example/boundservice18102021/MyService.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -14,65 +15,75 @@ import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
 
-    String CHANNEL_ID = "CHANNEL_ID";
-    int REQUEST_CODE_MINUS = 1;
-    int REQUEST_CODE_PLUS = 2;
-    Notification mNotification;
-    int mCount = 0;
+    private String CHANNEL_ID = "CHANNEL_ID";
+    private int REQUEST_CODE_MINUS = 1;
+    private int REQUEST_CODE_PLUS = 2;
+    private Notification mNotification;
+    private int mCount = 0;
     NotificationManager mNotificationManager;
+
+    class MyBinder extends Binder {
+        MyService getService() {
+            return MyService.this;
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return new MyBinder();
     }
+
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mNotification = createNotification(this,"Count : " + mCount);
-        startForeground(1,mNotification);
+        mNotification = createNotification(this, "Count : " + mCount);
+        startForeground(1, mNotification);
         mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        mNotificationManager.notify(1,mNotification);
-        Log.d("BBB","onCreate");
+        mNotificationManager.notify(1, mNotification);
+        Log.d("BBB", "onCreate");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("BBB","onStartCommand");
-        if (intent != null){
-            mCount += intent.getIntExtra("count",0);
-            mNotification = createNotification(this,"Count : " + mCount);
-            mNotificationManager.notify(1,mNotification);
+        Log.d("BBB", "onStartCommand");
+        if (intent != null) {
+            mCount += intent.getIntExtra("count", 0);
+            mNotification = createNotification(this, "Count : " + mCount);
+            mNotificationManager.notify(1, mNotification);
         }
         return START_NOT_STICKY;
     }
 
     @Override
     public void onDestroy() {
-        Log.d("BBB","onDestroy");
+        Log.d("BBB", "onDestroy");
         super.onDestroy();
     }
 
-    private Notification createNotification(Context context, String message){
-        Intent intentMinus = new Intent(context,MyService.class);
-        intentMinus.putExtra("count",-1);
+    private Notification createNotification(Context context, String message) {
+        Intent intentMinus = new Intent(context, MyService.class);
+        intentMinus.putExtra("count", -1);
 
-        PendingIntent pendingIntentMinus = PendingIntent.getService(context,REQUEST_CODE_MINUS,intentMinus,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntentMinus = PendingIntent.getService(context, REQUEST_CODE_MINUS, intentMinus, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        Intent intentPlus = new Intent(context,MyService.class);
-        intentPlus.putExtra("count",1);
+        Intent intentPlus = new Intent(context, MyService.class);
+        intentPlus.putExtra("count", 1);
 
-        PendingIntent pendingIntentPlus = PendingIntent.getService(context,REQUEST_CODE_PLUS,intentPlus,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntentPlus = PendingIntent.getService(context, REQUEST_CODE_PLUS, intentPlus, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,CHANNEL_ID);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
         builder.setContentTitle("Thông báo");
         builder.setContentText(message);
         builder.setSmallIcon(R.mipmap.ic_launcher);
         builder.setShowWhen(true);
-        builder.addAction(R.mipmap.ic_launcher_round,"Plus",pendingIntentPlus);
-        builder.addAction(R.mipmap.ic_launcher,"Minus",pendingIntentMinus);
-
+        builder.addAction(R.mipmap.ic_launcher_round, "Plus", pendingIntentPlus);
+        builder.addAction(R.mipmap.ic_launcher, "Minus", pendingIntentMinus);
 
         return builder.build();
+    }
+    public int getCount(){
+        return mCount;
     }
 }
